76009550121c834b0dbd7f62e62db45dfa4fbb71
Evgeniy-Shtykhlin-EPAM <Evgeniy_Shtykhlin@epam.com>
2023-06-19 17:28:15 +0600

    finished

--- Tasks/DoublyLinkedList.cs
+++ Tasks/DoublyLinkedList.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Linq;
 using Tasks.DoNotChange;
 
 namespace Tasks
@@ -15,19 +14,14 @@ namespace Tasks
         public T Data { get; }
         public Node <T> Next { get; set; }
         public Node <T> Previous { get; set; }
-        public int Index { get; set; }
+        public int? Index { get; set; }
     }
 
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
         public Node<T> Head { get; set; }
         public Node<T> Tail { get; set; }
-
-        public Node<T> First { get; set; }
-        public Node<T> Last { get; set; }
-
         public int Length { get; set; }
-
 
         public void Add(T e)
         {
@@ -35,6 +29,7 @@ namespace Tasks
             if (Tail == null)
             {
                 Head = newNode;
+                Head.Index = 0;
             }
             else
             {
@@ -47,16 +42,55 @@ namespace Tasks
             Length++;
         }
 
-
         public void AddAt(int index, T e)
         {
-            
+            if (Length == index)
+            {
+                Node<T> temp = new Node<T>(e);
 
+                temp.Index = index;
+                temp.Next = null;
+                temp.Previous = Tail;
+
+                Tail.Next = temp;
+
+                Length++;
+
+            }
+            else
+            {
+                Node<T> current = Head;
+                while (current.Index != index)
+                {
+                    current = current.Next;
+                }
+
+                Node<T> temp = new Node<T>(e);
+
+                temp.Index = index;
+                temp.Next = current.Next;
+                temp.Previous = current;
+
+                current.Index = index - 1;
+                current.Next = temp;
+                Length++;
+            }
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            if (Length == 0 || index >= Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node<T> node = Head;
+            while (node.Index != index)
+            {
+                node = node.Next;
+            }
+
+            return node.Data;
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -71,12 +105,55 @@ namespace Tasks
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            Node<T> current = Head;
+
+            while (current != Tail)
+            {
+                current = current.Next;
+
+                if (current.Data.Equals(item))
+                {
+                    Node<T> prev = current.Previous;
+                    prev.Next = null;
+                    current.Previous = null;
+
+                    Length--;
+                }
+            }
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (Length == 0 || index >= Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node<T> currentTemp = Head;
+
+            while (currentTemp.Index != index)
+            {
+                currentTemp = currentTemp.Next;
+            }
+
+
+            Node<T> prev = currentTemp.Previous;
+
+            if (currentTemp.Next == null)
+            {
+                prev.Next = null;
+            }
+            else
+            {
+                Node<T> next = currentTemp.Next;
+
+                prev.Next = next;
+                next.Previous = prev;
+                Tail = currentTemp;
+            }
+            Length--;
+
+            return currentTemp.Data;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
--- Tasks/HybridFlowProcessor.cs
+++ Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,68 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (Head == null)
+            {
+                throw new InvalidOperationException();
+
+            }
+            Node<T> node = Head;
+
+            Node<T> nextNode = Head.Next;
+
+            nextNode.Previous = null;
+
+            Head = nextNode;
+
+            return node.Data;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newNode = new Node<T>(item);
+
+            if (Tail == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                newNode.Previous = Tail;
+                Tail.Next = newNode;
+            }
+
+            Tail = newNode;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (Tail == null)
+            {
+                throw new InvalidOperationException();
+            }
+            return Tail.Data;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newNode = new Node<T>(item);
+
+            if (Tail == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                newNode.Previous = Tail;
+                Tail.Next = newNode;
+            }
+
+            Tail = newNode;
         }
     }
 }
